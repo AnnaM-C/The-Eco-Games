5caccd4caf88bc90e1126e832974d11d83a2d448
Melric Lazaro <melriclazaro@gmail.com>
2023-03-29 16:42:23 +0100

    Added Tips Page

--- theEcoGames/game/urls.py
+++ theEcoGames/game/urls.py
@@ -6,10 +6,16 @@ app_name = "gameapp"
 urlpatterns = [
     # Profile View
     path('', views.profile, name ='profile'),
+
     # Leaderboard View
     path('leaderboards', views.leaderboards, name='leaderboards'),
+
     # Maps View
     path('maps', views.maps, name = 'maps'),
+
     # Activities View
     path('activities', views.createActivitiesView, name = 'activities'),
+    
+    # Tips View
+    path('tips', views.tipsIndex, name = 'tips'),
 ]
